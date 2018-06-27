dec44773814af1f44c5d408807b69177b9e48dfb
raphaelli <raphael_li@live.com>
2018-06-27 23:32:31 +0800

    添加XML 内容协商服务，可以通过设置请求头为 xml 来请求XML 信息

--- SdnoteServer/Startup.cs
+++ SdnoteServer/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace SdnoteServer
@@ -10,7 +11,13 @@ namespace SdnoteServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(); // 注册MVC到Container
+             // 注册MVC到Container
+            services.AddMvc()
+                .AddMvcOptions(options =>
+                {
+                    //添加XML 内容协商服务
+                    options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                }); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
