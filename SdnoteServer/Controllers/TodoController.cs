4ace98644b80d933d2e76959e77901ca7ed10fff
raphaelli <raphael_li@live.com>
2018-06-28 00:11:50 +0800

    添加post 请求新加todo ， error： 时间类型未处理

--- SdnoteServer/Controllers/TodoController.cs
+++ SdnoteServer/Controllers/TodoController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SdnoteServer.Service;
 using System.Linq;
+using SdnoteServer.Dto;
 
 namespace SdnoteServer.Controllers
 {
@@ -13,7 +14,7 @@ namespace SdnoteServer.Controllers
             return Ok(TodoService.Current.Todos);
         }
 
-        [Route("{id}")]
+        [Route("{id}", Name ="GetTodo")]
         public IActionResult GetTodo(int id)
         {
             var todo = TodoService.Current.Todos.SingleOrDefault(x => x.Id == id);
@@ -23,6 +24,28 @@ namespace SdnoteServer.Controllers
             }
 
             return Ok(todo);
+        }
+
+        //POST 添加todo
+        [HttpPost]
+        public IActionResult Post([FromBody] TodoCreation todo)
+        {
+            if (todo == null)
+            {
+                return BadRequest();
+            }
+
+            var maxId = TodoService.Current.Todos.Max(x => x.Id);
+            var newTodo = new Todo()
+            {
+                Id = ++maxId, //ID 顺位加一
+                TdName = todo.TdName,
+                TdTime = todo.TdTime
+            };
+
+            TodoService.Current.Todos.Add(newTodo);
+
+            return CreatedAtRoute("GetTodo",new {id = newTodo.Id, newTodo });
         }
     }
 }
---
+++ SdnoteServer/Dto/TodoCreation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SdnoteServer.Dto
+{
+    public class TodoCreation
+    {
+        public string TdName { get; set; }
+        public DateTime TdTime { get; set; }
+    }
+}
